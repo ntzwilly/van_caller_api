90390b02caf298589b068d9c7b7f5f60f9b61565
ntzwilly <ntzwilly@gmail.com>
2023-07-14 13:56:50 +0200

    Add default values

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
-# name = "360 Acadie Avenue Apartments"
-# address = "360, Acadie Ave, Dieppe, Canada"
-# Location.create(name: name, address: address)
+addresses = ["28 Elizabeth St", "2-42 Brandon St", "64 Wilbur St", "70 Derby St", "74 Humphrey St", " 105 Leslie St", "82 Walsh St"]
 
-# name = "Wags To Whiskers"
-# address = "203, Rue Ste Therese, Dieppe, Canada"
-# Location.create(name: name, address: address)
-
-# name = "40 Mathieu Crescent"
-# address = "40, Mathieu Crescent"
-# Location.create(name: name, address: address)
-
-# name = "78 Neville St"
-# address = "78, Neville St, Moncton, Canada"
-# Location.create(name: name, address: address)
-
-# name = "25 Carl St"
-# address = "25, Carl St, Moncton, Canada"
-# Location.create(name: name, address: address)
-
-# name = "107 Winter Ave"
-# address = "107, Winter Ave, Moncton, Canada"
-# Location.create(name: name, address: address)
+addresses.each { |address| Location.create(street: address) }
 
